f4c22ab9fdb0ac12d46889acd0fefc9936085c83
Jisun696 <istiakkashem35@gmail.com>
2024-08-17 18:31:57 +0600

    added products by fetching data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,27 @@
+import ProductCard from '@/components/ProductCard';
+import useAxiosPublic from '@/hooks/useAxiosPublic';
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+  const axiosPublic = useAxiosPublic();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axiosPublic.get('/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, [axiosPublic]);
   return (
-    <div>
-      <h1 className="text-center">HOMEPAGE</h1>
+    <div className=" container grid grid-cols-4 gap-5">
+      {products?.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 };
